d33406f40ef47922e0273d1a04f28f3a716efcb1
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2024-01-09 10:47:02 +0100

    Commit 1

---
+++ IeIAPI/IeIAPI/Startup.cs
@@ -0,0 +1,86 @@
+﻿
+using Microsoft.Extensions.Options;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace IeIAPI
+{
+    
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+         /*   services.Configure<FormOptions>(options =>
+{
+    options.ValueCountLimit = int.MaxValue;
+    options.MultipartBodyLengthLimit = long.MaxValue;
+    options.MemoryBufferThreshold = int.MaxValue;
+});*/
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowRender",
+                    builder => builder.AllowAnyOrigin()
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader());
+            });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configuración de la base de la ruta si es necesario
+        
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseCors("AllowRender");
+
+                // Agregar registros de depuración
+                app.Use(async (context, next) =>
+                {
+                    Console.WriteLine($"Request: {context.Request.Path}");
+                    await next.Invoke();
+                });
+            }
+
+            // Configuración adicional...
+
+            app.UseCors("AllowRender");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+
+}
+
+
