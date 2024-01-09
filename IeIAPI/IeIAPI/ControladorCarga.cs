0ff609c653a51a748ce339837a1adecae8ae5a68
Valentino Dominguez Rabanal <VDOMRAB@upv.edu.es>
2024-01-09 12:28:34 +0100

    uysdgfaygjdajsh

--- IeIAPI/IeIAPI/ControladorCarga.cs
+++ IeIAPI/IeIAPI/ControladorCarga.cs
@@ -163,35 +163,6 @@ namespace IeIAPI
 
                 string json = Extractor2XML.Extractor2(numeros, doc);
 
-
-                Console.WriteLine("data" + json);
-                try
-                {
-                    List<string> c_e = new List<string>();
-                    List<int> pr = new List<int>();
-                    List<int> loc = new List<int>();
-                    dynamic[] dataArray = JsonConvert.DeserializeObject<dynamic[]>(json);
-
-                    Console.WriteLine("Connection successful!");
-                    foreach (dynamic data in dataArray)
-                    {
-
-
-                        InsertIntoProvincia(connection, data);
-
-                        InsertIntoLocalidad(connection, data);
-
-                        InsertIntoCentroEducativo(connection, data);
-
-
-
-                    }
-                }
-                catch (SqlException ex)
-                {
-                    //Console.WriteLine("Error: " + ex.Message);
-                }
-
                 return Ok(json);
             }
         }
